42617a308646b823d0cdf10d74cc56414f7f3821
Jenna Qian <jiayin1996@gmail.com>
2021-04-28 13:06:35 -0400

    Add Buttons

---
+++ src/Components/App/index.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Button from "../Button";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="display">
+        <p>Results</p>
+      </div>
+      <div className="buttons">
+        <Button />
+        {/* <button className="button">AC</button>
+        <button className="button">±</button>
+        <button>%</button>
+        <button>+</button>
+        <button>−</button>
+        <button>÷</button>
+        <button>×</button>
+        <button>0</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>.</button>
+        <button>=</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
